63f1f4a717112fa15499fb5ab9969c7fbccba94d
avisheksanvas <avisheksanvas@gmail.com>
2018-03-17 07:44:13 +0530

    Initial commit

---
+++ Assets/bot/animationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour {
+
+	Animator anim ;
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	void Update () {
+		float vertical = Input.GetAxis ("Vertical");
+
+		if (vertical > 0)
+			anim.SetInteger ("state", 1);
+		else if(vertical < 0)
+			anim.SetInteger ("state", 2);		
+	}
+}
